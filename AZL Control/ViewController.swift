6d2f4f2ee68334ad6a9e8b5d0679b8685cf0e506
smflore4 <smflore4@asu.edu>
2017-06-11 18:07:52 -0500

    Initial Commit

---
+++ AZL Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AZL Control
+//
+//  Created by Stephen Flores on 6/11/17.
+//  Copyright © 2017 Stephen Flores. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
